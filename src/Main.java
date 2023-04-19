8eed66a812779e6bf4d4015051d3ed20d41bfa1f
DavidAH <cph-dh223@cphbusiness.dk>
2023-04-19 10:31:57 +0200

    bedere test names

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,18 @@ public class Main {
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 5; j++) {
                 
-                Car c1 = new DieselCar("Audi", "model" + j + "." + i, 2, "" , i == 0, i * 5);
+                Car c1 = new DieselCar("Audi", "model" + j + "." + i, 2, i+j + "D" , i == 0, i * 5);
                 fleet.addCar(c1);
-                Car c2 = new GasolinCar("Masda", "model", 4, "",  i*5);
+                Car c2 = new GasolinCar("Masda", "model", 4, i+j + "G",  i*5);
                 fleet.addCar(c2);
             }
         }
 
-        fleet.addCar(new ElectricCar("Tesla", "model1", 2, "", 2000, 1));
-        fleet.addCar(new ElectricCar("Tesla", "model2", 2, "", 1000, 1));
-        fleet.addCar(new ElectricCar("Tesla", "model3", 2, "", 850, 1));
-        fleet.addCar(new ElectricCar("Tesla", "model4", 2, "", 1000, 2));
-        fleet.addCar(new ElectricCar("Tesla", "model5", 2, "", 850, 2));
+        fleet.addCar(new ElectricCar("Tesla", "model1", 2, "1E", 2000, 1));
+        fleet.addCar(new ElectricCar("Tesla", "model2", 2, "2E", 1000, 1));
+        fleet.addCar(new ElectricCar("Tesla", "model3", 2, "3E", 850, 1));
+        fleet.addCar(new ElectricCar("Tesla", "model4", 2, "4E", 1000, 2));
+        fleet.addCar(new ElectricCar("Tesla", "model5", 2, "5E", 850, 2));
 
         System.out.println(fleet);
         System.out.println("Fleet cost: " + fleet.getTotalRegistrationFeeForFleet());
